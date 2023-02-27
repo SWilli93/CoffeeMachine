c4a8b9ad387807f6a1af83377b0a5535a338bacf
Scott <ScottWilliamsSolutions@gmail.com>
2023-02-27 16:34:40 -0500

    all Functionality

--- main.py
+++ main.py
@@ -33,11 +33,8 @@ resources = {
 
 #TODO 1: prompt user by asking what would you like?
 
-# user_request = input("What would you like? ")
-# if user_request:
-#     print("Ok here you go.")
-# else:
-#     print("machine is off")
+request = input("What would you like? ")
+
 
 # TODO 2: turn off the machine by typing off
 
@@ -46,28 +43,71 @@ turn_off = False
 
 #TODO 3: print report
 
-# if *arg == report:
-#     report = print(resources)
 
-'''this will be to compare ingredients'''
-espresso, latte, cappuccino = [menu[v] for v in menu]
-print(espresso)
+report = print(resources)
+print(report)
+# '''this will be to compare ingredients'''
+
+espresso, latte, cappuccino = [menu[v]["ingredients"] for v in menu]
+espresso_cost, latte_cost, cappuccino_cost = [menu[v]['cost'] for v in menu]
+espresso['milk'] = 0
 
 
+#TODO 4 check resources sufficient
 
-# for resources["amount"] in resources:
-#     if "amount" < drink:
-#         print("sorry inefficient amount of resources")
-#     else:
-#         print("ok here you go")
+
+def is_enough(request):
+    global resources, espresso, latte, cappuccino
+    if int(resources['water']) - int(request['water']) <= 0:
+        return True
+    elif resources['milk'] - request['milk'] <= 0:
+        return True
+    elif resources['coffee'] - request['coffee'] <= 0:
+        return True
+    else:
+        return False
+
+#TODO 5 Process Coins
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+#todo 6 transaction successful
+
+def is_successful(money, drink):
+    change = money - drink
+    if money < drink:
+        return print("insufficient funds"), False
+    elif money > drink:
+        answer = input("would you like change? y or n")
+        if answer == 'y':
+            return True, print(f"refund of ${change} incoming")
+        else:
+            return print("thankyou have a great day"), True
+    else:
+        return True, print("coffee incoming")
+
+
+#todo 7: Make the coffee
+
+
+if is_successful() and is_enough():
+    # print(report)
+    # print(new_report)
 
 
 # def check_response(request):
-#     global resources, user_request
+#     global resources, resources_left
 #     if request == "espresso":
-#         resources["water"] -= 50
-#         resources["coffee"] -= 18
-#         return resources
+#         espresso["water"] -= 50
+#         espresso["coffee"] -= 18
+#         return resources_left
 #     elif request == "latte":
 #         resources["water"] -= 200
 #         resources["milk"] -= 150
@@ -81,8 +121,8 @@ print(espresso)
 #     else:
 #         print("Please Enter Valid Selection")
 #     check_response(user_request)
-
-
+#
+#
 # check_response(user_request)
 
 
